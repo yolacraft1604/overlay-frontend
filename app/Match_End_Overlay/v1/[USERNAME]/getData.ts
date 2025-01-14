bcfe2e6c1c45528e7ec941feada4b2f706b29008
rubenb1604 <rubenbertram@gmx.de>
2025-01-14 20:24:00 +0100

    .env

--- app/Match_End_Overlay/v1/[USERNAME]/getData.ts
+++ app/Match_End_Overlay/v1/[USERNAME]/getData.ts
@@ -1,5 +1,6 @@
 export async function getData(username: string) {
-    const data = await fetch("http://localhost:5500/api/getMatch/" + username);
+    const url = process.env.BACKEND;
+    const data = await fetch(url + "/api/getMatch/" + username);
     const json = await data.json();
     return json;
 }
